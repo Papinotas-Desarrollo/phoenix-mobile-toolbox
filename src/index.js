42735c07ccafea7d8b8d3dfb950814fdcb54b24f
tmedina <tomasmedina.a@gmail.com>
2019-05-26 10:43:37 -0400

    chore: removed unnecesary imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 import colors from './styles/colors';
 import * as Login from './components/Login';
 import { ErrorBoundaryWrapper } from './components/ErrorBoundary';
-import { HelpAccordion, sendEmail } from './components/Help';
+import { HelpAccordion } from './components/Help';
 
-export { colors, Login, ErrorBoundaryWrapper, HelpAccordion, sendEmail };
+export { colors, Login, ErrorBoundaryWrapper, HelpAccordion };

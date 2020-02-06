b785d333ba0427c161cd88b4621ec0bd174689cd
Julio Orellana <julio@papinotas.com>
2020-02-06 17:39:44 -0300

    chore: enhance accordion

--- typings/components/Accordion.d.ts
+++ typings/components/Accordion.d.ts
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 
+type customExpandIconProps = {
+  openName: string;
+  closeName: string
+}
+
 export interface AccordionProps {
   children: React.ReactNode;
   title: React.ReactNode;
@@ -11,6 +16,10 @@ export interface AccordionProps {
   style?: StyleProp<ViewStyle>;
   theme?: ThemeShape;
   iconColor?: string;
+  containerStyle?: any;
+  customExpandIcon?: React.ComponentType<customExpandIconProps>;
+  right?: React.ComponentType;
+  hideBorderBottomWidth?: boolean;
 }
 
 export declare class Accordion extends React.Component<AccordionProps> {}
